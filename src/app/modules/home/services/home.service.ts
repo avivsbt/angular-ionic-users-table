257be82a12285c38f6f0c4ef333145fbf0e3a991
avivsbt <49982869+avivsbt@users.noreply.github.com>
2024-02-12 12:24:19 +0200

    [Api] get users

--- src/app/modules/home/services/home.service.ts
+++ src/app/modules/home/services/home.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, Subject, map } from 'rxjs';
+import { Subject } from 'rxjs';
 import { IUser, IUsersResponseDto } from '../models/users';
 
 @Injectable({
@@ -10,15 +10,26 @@ export class HomeService {
 
   constructor(private http: HttpClient) { }
 
-  private readonly _users = new Subject<Map<string, IUser>>();
+  private readonly _users = new Subject<Map<string, IUser>>(); //
   readonly users$ = this._users.asObservable();
 
+  public async loadUsers(): Promise<void> {
+    try {
+      const usersLocalStorage = await localStorage.getItem('users');
+      if (usersLocalStorage) {
+        this._users.next(new Map(JSON.parse(usersLocalStorage)));
+        return;
+      }
+      this.getUsersApi();
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
 
-  public loadUsers(): void {
+  private getUsersApi(): void {
     this.http.get("https://randomuser.me/api/?page=1&results=50").subscribe(response => {
 
       const usersResponse = response as IUsersResponseDto;
-
       const users: Map<string, IUser> = new Map();
 
       usersResponse.results.forEach(obj => {
